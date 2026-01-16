9cf141d9706a8e6aa233c40e30b3abe83f044ce9
FergusJJ <fergus@joinblink.com>
2026-01-16 16:22:33 +0000

    feat: add ethers, colors, banner

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import Banner from "./components/Banner";
+
+/*
+ * Need:
+ * Landing page
+ * - tagline
+ * - nav
+ *   - connect to metamask tr
+ *   - title tl
+ * - display current yield
+ *   - want some copy in middle with what the project is about
+ * - probably want some reusable btn components
+ *
+ *
+ * */
 
 function App() {
   return (
     <>
       <div>
-        <p>Hello world</p>
+        <Banner />
       </div>
     </>
   );
---
+++ web/src/components/Banner.tsx
@@ -0,0 +1,10 @@
+import s from "./banner.module.css";
+
+export default function Banner() {
+  return (
+    <nav className={s.banner}>
+      <p>banner elem 1</p>
+      <p>Banner elem 2</p>
+    </nav>
+  );
+}
