b6da343f60443d28c38e9abb727e7a23da9e87b7
FergusJJ <fergus@joinblink.com>
2026-01-16 20:40:41 +0000

    feat: get started btn wip

--- web/src/components/Home/Home.tsx
+++ web/src/components/Home/Home.tsx
@@ -1,7 +1,51 @@
-export default function Home() {
+import client from "../../util/client";
+import s from "./Home.module.css";
+import { useActiveWallet, useConnectModal } from "thirdweb/react";
+import { useNavigate } from "react-router-dom";
+
+function HomeCopy() {
+  const title = "Delta0";
+  const tagline =
+    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur";
+  return (
+    <div className={s.copyWrapper}>
+      <p className={s.title}>{title}</p>
+      <p className={s.tagline}>{tagline}</p>
+    </div>
+  );
+}
+
+function GetStarted() {
+  const navigate = useNavigate();
+  const wallet = useActiveWallet();
+  const { connect, isConnecting } = useConnectModal();
+  const handleClick = () => {
+    if (!wallet) {
+      void handleConnect();
+      return;
+    }
+    navigate("/screen-2");
+  };
+  const handleConnect = async () => {
+    const wallet = await connect({ client }); // opens the connect modal
+    console.log("connected to", wallet);
+  };
+
+  const label = "Get Started";
   return (
     <div>
-      <p> Home</p>
+      <button disabled={isConnecting} onClick={handleClick}>
+        {label}
+      </button>
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <div className={s.homeWrapper}>
+      <HomeCopy />
+      <GetStarted />
     </div>
   );
 }
