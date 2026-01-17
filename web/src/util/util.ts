09b6bbb0d8f1508d6dd997b8ebc02a0d7783ba38
FergusJJ <fergus@joinblink.com>
2026-01-17 03:18:59 +0000

    feat: add wagmi conf, calculate yield

---
+++ web/src/util/util.ts
@@ -0,0 +1,20 @@
+import { getBalance } from "@wagmi/core";
+import { config } from "@/web/src/config/wagmi";
+import { formatEther } from "viem";
+
+export async function calculateYield({
+  address,
+  fundingRate,
+  totalLiquidity,
+}: {
+  address: string;
+  fundingRate: number;
+  totalLiquidity: number;
+}) {
+  const addressEthBal = await getBalance(config, {
+    address: `0x${address}`,
+  });
+  const ethValue = Number(formatEther(addressEthBal.value));
+  const result = (fundingRate * ethValue) / totalLiquidity;
+  return result;
+}
